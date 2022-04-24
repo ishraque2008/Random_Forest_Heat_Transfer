890cedeea056807be521b45a9feae9410c541802
Ishraque Borshon <ishraque@okstate.edu>
2022-04-24 17:50:36 -0500

    Varied Mass Flowrate

--- main.py
+++ main.py
@@ -14,21 +14,19 @@ from sklearn.preprocessing import LabelEncoder
 import xgboost as xgb
 import numpy as np
 
-st.header("CSP Receiver Heat Loss Prediction Software")
-st.write('Author: Ishraque Zaman Borshon')
-st.write('Source: \"Borshon, I. Z., 2021. Study of Upward facing cavity receiver for Scheffler\'s Concentrator. '
-         'M.Tech. Thesis, Indian Institute of Technology, Bombay.\" ')
-name = st.text_input("Enter your Name: ", key="name")
-
-
-
 # Importing the dataset
 dataset = pd.read_csv('Nu_Correlation_Physical_Based_12_removed.csv')
 X = dataset.iloc[:, 1:15].values
 y = dataset.iloc[:, 15:18].values
 
+
+st.header("CSP Receiver Heat Loss Prediction Software")
+st.write('Author: Ishraque Zaman Borshon')
+st.write('Source: \"Borshon, I. Z., 2021. Study of Upward facing cavity receiver for Scheffler\'s Concentrator. '
+         'M.Tech. Thesis, Indian Institute of Technology, Bombay.\" ')
+name = st.text_input("Enter your Name: ", key="name")
 if st.checkbox('Show dataframe'):
-    dataset.show()
+    dataset.head()
 
 def Random_Forest():
     # Splitting the dataset into the Training set and Test set
@@ -103,5 +101,11 @@ def Random_Forest():
         st.write('I hope the software was helpful.')
 
 
+    # inputs = np.expand_dims(
+    #     [0,9.81,0.5,0.3,0.6,300,723,511.5,0.001955034,0.7,0.00004765,0.5,0.19635,3.444845],
+    #     0)
+    # prediction = regressor.predict(inputs)
+    # conv_loss = prediction[0][0]
+    # print(conv_loss)
 if __name__ == '__main__':
     Random_Forest()
